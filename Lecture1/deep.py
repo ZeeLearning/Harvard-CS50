ef9bc293a38d74fc8dd7c5b139d8a040b6d01b8b
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-07-21 03:01:43 -0400

    Apply space and case insensitivity

--- Lecture1/deep.py
+++ Lecture1/deep.py
@@ -1,5 +1,8 @@
 user_question = input("What is the answer to the Great Question of Life, the Universe and Everything?")
 
+# Apply space and case insensitivity
+user_question = user_question.strip().lower()
+
 # Conditional Logic 
 if user_question == "42" or user_question == "forty two" or user_question == "forty-two":
     print("Yes")

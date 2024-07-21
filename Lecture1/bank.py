75acc078d6ef13470753637f32756e9523b17efb
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-07-21 03:40:36 -0400

    Added logic for space and case insensitivity

--- Lecture1/bank.py
+++ Lecture1/bank.py
@@ -1,10 +1,16 @@
 # Greeting from user 
 greeting = input("Greeting: ")
 
+# Apply space and case insensitivity
+greeting = greeting.strip().lower()
+
+prize_greeting = "hello"
+
 # Conditional logic 
-if greeting.startswith("h") and greeting != "hello":
-    print("$20")
-elif greeting == "hello":
+if greeting == "hello" or "hello" in greeting:
     print("$0")
+elif greeting.startswith("h") and greeting != "hello":
+    print("$20")
 else:
     print("$100")
+

3e2bb48f6ebaaf1ca6aa8a0e356310bca00831b1
Hukam <hukam@zoofle.local>
2024-07-30 02:37:28 -0400

    Updated coke machine assignment of Lecture_2

--- Lecture_2/coke.py
+++ Lecture_2/coke.py
@@ -8,12 +8,29 @@ output how many cents in change the user is owed. Assume that the user will only
 and ignore any integer that isn’t an accepted denomination.
 '''
 
-amount_due = int(50)
+# Accepted denomications 
+accepted_coins = [25,10,5]
+# Total cost of coke 
+cost = 50 
+# Inserted Amount 
+total_amount = 0
 
-print(f"Amount Due: {amount_due}")
+while total_amount < cost:
 
-# Prompt user to inert coin
-insert_coin = int(input("Insert Due: "))
+    amount_due = cost - total_amount
+    # Print amount due 
+    print(f"Amount Due: {amount_due}")
 
-print(insert_coin)
+    # Prompt user to insert coin 
+    coin = int(input("Insert Coin: "))
 
+    # Check if inserted coin is in accepted denomication 
+    if coin in accepted_coins:
+        total_amount += coin 
+    else:
+        break
+
+    if total_amount >= 50:
+        changed_owed = total_amount - cost
+
+        print(f"Change Owed: {changed_owed}")

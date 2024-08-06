cdc90a97d68650a9cb42a510767fd54dcc8b32d1
Hukam <hukam@zoofle.local>
2024-08-06 03:08:30 -0400

    Added nutrition assignment of Lecture_2

---
+++ Lecture_2/nutrition.py
@@ -0,0 +1,34 @@
+# Dictionary for fruits and their corresponding calories
+nutrition_table = {
+    "Apple": 130,
+    "Avocado": 50,
+    "Banana": 110,
+    "Cantaloupe": 50,
+    "Grapefruit": 60,
+    "Grapes": 90,
+    "Honeydew Melon": 50,
+    "Kiwifruit": 90,
+    "Lemon": 15,
+    "Lime": 20,
+    "Nectarine": 60,
+    "Orange": 80,
+    "Peach": 60,
+    "Pear": 100,
+    "Pineapple": 50,
+    "Plums": 70,
+    "Strawberries": 50,
+    "Sweet Cherries": 100,
+    "Tangerine": 50,
+    "Watermelon": 80
+}
+
+# print(nutrition_table)
+
+# Prompt user to enter fruit 
+fruit = str(input("Item: "))
+
+# Check if user provided text is in the dictionary 
+if fruit in nutrition_table:
+    print(f"Calories: {nutrition_table[fruit]}")
+
+

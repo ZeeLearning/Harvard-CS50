f6b73715289d06a8c8d224296ae9f1fc83f66128
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-08-07 22:26:46 -0400

    Updated if statement to dispay Amount Due when inserted coin isn't in accepted coins list

--- Lecture_2/coke.py
+++ Lecture_2/coke.py
@@ -1,13 +1,3 @@
-'''
-Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in these denominations: 
-25 cents, 10 cents, and 5 cents.
-
-In a file called coke.py, implement a program that prompts the user to insert a coin, one at a time, 
-each time informing the user of the amount due. Once the user has inputted at least 50 cents, 
-output how many cents in change the user is owed. Assume that the user will only input integers, 
-and ignore any integer that isn’t an accepted denomination.
-'''
-
 # Accepted denomications 
 accepted_coins = [25,10,5]
 # Total cost of coke 
@@ -28,6 +18,7 @@ while total_amount < cost:
     if coin in accepted_coins:
         total_amount += coin 
     else:
+        print(f"Amount Due: {cost}")
         break
 
     if total_amount >= 50:

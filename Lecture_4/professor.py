cbe08515a369a141515cd2dee34961430f6ae3cc
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-08-28 10:51:52 -0400

    Added Little Professor assignment of Lecture_04

---
+++ Lecture_4/professor.py
@@ -0,0 +1,56 @@
+import random
+
+def main():
+    level = get_level()
+    # Initialize score 
+    score = 0
+
+    for _ in range(10):
+        x = generate_integer(level)
+        y = generate_integer(level)
+        correct_answer = x + y 
+
+        attempts = 0 
+
+        while attempts < 3:
+            try: 
+                answer = int(input(f"{x} + {y} = "))
+                if answer == correct_answer:
+                    score += 1
+                    break 
+                else:
+                    print("EEE")
+            except ValueError:
+                print("EEE")
+            attempts += 1 
+
+            if attempts == 3:
+                print(f"{x} + {y} = {correct_answer}")
+
+    print(f"Score = {score}/10")
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in [1, 2, 3]:
+                return level 
+        except ValueError:
+            pass 
+
+def generate_integer(level):
+    if level == 1:
+        # Generate single digit number 
+        return random.randint(0, 9)
+    elif level == 2:
+        # Generate two digits number 
+        return random.randint(10, 99)
+    elif level == 3:
+        # Generate three digits number
+        return random.randint(100,999)
+    else:
+        raise ValueError("Level must be 1, 2, or 3")
+
+
+if __name__ == "__main__":
+    main()

d06d07fd1cbfd58eed91232b3027b009f6efe439
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-09-02 03:39:00 -0400

    Added logic to handle invalid font

--- Lecture_4/figlet.py
+++ Lecture_4/figlet.py
@@ -2,29 +2,28 @@ import sys
 from pyfiglet import Figlet
 import random
 
-normal_font = input("Input: ")
-
 figlet = Figlet()
 
 if len(sys.argv) == 1:
+    normal_font = input("Input: ")
+
     fonts = figlet.getFonts()
     font = random.choice(fonts)
     figlet = Figlet(font = font)
 
     print(f"Output: \n{figlet.renderText(normal_font)}")
-
 elif len(sys.argv) == 3:
-        # Check if the first argument is -f or --font
-        if sys.argv[1] in ['-f', '--font']:
+        # Check if the first argument is -f or --font and second argument is valid font
+        if sys.argv[1] in ['-f', '--font'] and sys.argv[2] != 'invalid_font' :
             font = sys.argv[2]  # The second argument should be the name of the font
             try:
+                normal_font = input("Input: ")
                 figlet = Figlet(font=font)
                 print(f"Output: \n{figlet.renderText(normal_font)}")
             except:
                 sys.exit("Invalid usage")
         else:
-             sys.exit("Invalid usage")
-            
+             sys.exit("Invalid usage")           
 else:
      if len(sys.argv) > 3 or len(sys.argv) == 2:
         sys.exit("Invalid usage")

f29c244daff00a11023da0061f0cfe05829c5425
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-09-10 01:24:06 -0400

    Updated for unit testing.

--- Lecture_2/twttr.py
+++ Lecture_2/twttr.py
@@ -1,17 +1,27 @@
-# List of Vowels
-vowels = ['A','E','I','O','U','a','e','i','o','u']
-# Prompt user to enter a text 
-user_text = str(input("Input: "))
-# Define an empty string 
-output_text = ""
+def main():
+    # Prompt user to enter a text 
+    user_text = str(input("Input: "))
+    word_no_vowels = shorten(user_text)
 
-# Loop through text entered by user
-for char in user_text:
-    # Check if character is in list of vowels 
-    if char in vowels:
-        # Omitt the character
-        output_text += ''
-    else:
-        output_text += char
+    print(f"Output: {word_no_vowels}")
 
-print(f"Output: {output_text}")
+def shorten(word):
+    # List of Vowels
+    vowels = ['A','E','I','O','U','a','e','i','o','u']
+    # Define an empty string 
+    output_text = ""
+
+    # Loop through word
+    for char in word:
+        # Check if character is in list of vowels 
+        if char in vowels:
+            # Omitt the character
+            output_text += ''
+        else:
+            output_text += char
+
+        #print(f"Output: {output_text}")
+    return output_text
+    
+if __name__ == "__main__":
+    main()

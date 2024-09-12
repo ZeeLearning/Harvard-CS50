cccac26ee8cf017bb91254bbd45e8a0172427629
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-09-12 01:47:00 -0400

    Added two more tests.

--- Lecture_5/test_twttr.py
+++ Lecture_5/test_twttr.py
@@ -6,3 +6,9 @@ def test_shorten_uppercase_vowels():
 
 def test_shorten_lowercase_vowels():
     assert shorten("hello") == "hll"
+
+def test_shorten_empty():
+    assert shorten("") == ""
+
+def test_shorten_numbers_and_symbols():
+    assert shorten("12345!@#") == "12345!@#"

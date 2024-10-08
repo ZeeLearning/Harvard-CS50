4b039978c54db65fb0d33f26775bc4222c2ce4de
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-10-08 02:24:42 -0400

    Added more tests

--- Lecture_7/test_numb3rs.py
+++ Lecture_7/test_numb3rs.py
@@ -5,6 +5,11 @@ def test_IPV4_not_number():
 
 def test_IPV4():
     assert validate("1.2.3.4") == True
+    assert validate("192.168.1.1") == True
+    assert validate("0.255.255.255") == True
+    assert validate("255.0.0.1") == True
 
-def test_IPV4_not_255():
-    assert validate("275.3.6.28") == False
+def test_IPV4_out_of_range():
+    assert validate("300.100.50.25") == False
+    assert validate("1.300.299.298") == False
+

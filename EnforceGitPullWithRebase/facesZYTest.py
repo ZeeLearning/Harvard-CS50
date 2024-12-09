cd52c7f9861939447c159b1bccd6c55cd0d123a6
Zeeshan Yousaf <zyousaf31@outlook.com>
2024-12-09 16:52:28 -0500

    Added a folder for testing how to enforce git pull with rebase.

---
+++ EnforceGitPullWithRebase/facesZYTest.py
@@ -0,0 +1,20 @@
+def convert(text):
+    # Replace ":)" with 🙂
+    text = text.replace(":)", "🙂")
+    
+    # Replace ":(" with 🙁
+    text = text.replace(":(", "🙁")
+
+    return text 
+
+def main():
+    # Get input from user 
+    user_text = input('Please enter text: ')
+    # Call convert function 
+    converted_text = convert(user_text)
+    # Print the result 
+    print(converted_text)
+
+# Call main 
+if __name__ == "__main__":
+    main()
